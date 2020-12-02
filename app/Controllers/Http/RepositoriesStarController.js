c91c2256ce78722f1cf630661489674da3cc0af3
infojanio <you@example.com>
2020-12-02 18:36:37 -0300

    crud stars

--- app/Controllers/Http/RepositoriesStarController.js
+++ app/Controllers/Http/RepositoriesStarController.js
@@ -1,92 +1,50 @@
 'use strict'
 
-/** @typedef {import('@adonisjs/framework/src/Request')} Request */
-/** @typedef {import('@adonisjs/framework/src/Response')} Response */
-/** @typedef {import('@adonisjs/framework/src/View')} View */
+const RepositoriesStar = use('App/Models/RepositoriesStar')
 
-/**
- * Resourceful controller for interacting with repositoriesstars
- */
 class RepositoriesStarController {
-  /**
-   * Show a list of all repositoriesstars.
-   * GET repositoriesstars
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async index ({ request, response, view }) {
+  // listar as estrelas de um determinado repositório
+  async index ({ params }) { // params serve p/ mostrar apenas as stars do projeto escolhido
+    const star = await RepositoriesStar.query()
+      .where('repository_id', params.repositories_id)
+      .with('user')
+      .fetch()
+
+    return star
   }
 
-  /**
-   * Render a form to be used for creating a new repositoriesstar.
-   * GET repositoriesstars/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
+  async store ({ params, request }) {
+    const data = request.only([
+      'user_id',
+      'repository_id',
+      'count'
+    ])
+
+    const star = await RepositoriesStar.create({ ...data, repository_id: params.repositories_id })
+    return star
   }
 
-  /**
-   * Create/save a new repositoriesstar.
-   * POST repositoriesstars
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async store ({ request, response }) {
+  async show ({ params }) {
+    const star = await RepositoriesStar.findOrFail(params.id)
+    return star
   }
 
-  /**
-   * Display a single repositoriesstar.
-   * GET repositoriesstars/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async show ({ params, request, response, view }) {
+  async update ({ params, request }) {
+    const star = await RepositoriesStar.findOrFail(params.id)
+    const data = request.only([
+      'user_id',
+      'repository_id',
+      'count'
+    ])
+
+    star.merge(data)
+    await star.save()
+    return star
   }
 
-  /**
-   * Render a form to update an existing repositoriesstar.
-   * GET repositoriesstars/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
-  }
-
-  /**
-   * Update repositoriesstar details.
-   * PUT or PATCH repositoriesstars/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async update ({ params, request, response }) {
-  }
-
-  /**
-   * Delete a repositoriesstar with id.
-   * DELETE repositoriesstars/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async destroy ({ params, request, response }) {
+  async destroy ({ params }) {
+    const star = await RepositoriesStar.findOrFail(params.id)
+    await star.delete()
   }
 }
 
--- start/routes.js
+++ start/routes.js
@@ -14,4 +14,6 @@ Route.post('passwords', 'ForgotPasswordController.store')
 // Rotas acessíveis apenas quando o usuário estiver logado
 Route.group(() => {
   Route.resource('repositories', 'RepositoryController').apiOnly()
+
+  Route.resource('repositories.stars', 'RepositoriesStarController').apiOnly() // só tem stars se tiver repositories
 }).middleware(['auth']) // chama todos as rotas possíveis
